b3642aaa390d55b9066d24e750f76334e4a5dbe2
Vikaskumarrr <kashyupvikas2@gmail.com>
2025-01-15 02:19:52 +0530

    useRef hook

--- jan12/src/App.js
+++ jan12/src/App.js
@@ -4,24 +4,38 @@
  * Authorization app (realtime login & signup flow )
  */
 
+// useref => creates an object in the frist render of the component and server the same object acess all other re render
+// useRef do not consider the value passed from second render onwords 
+// `useRef` do not consider the value passed from second render onwords 
 
-import { useState } from "react";
+// useState => useState can be used when the data is mutable and we need render for every change in the data 
+// useRef => useRef can be used to single memory reference access multiple renderof the comonent. 
+
+
+
+
+import { useRef, useState } from "react";
 
 const arr = [];
 
 function App() {
   const [count, setCount] = useState(10);
-  let obj = { name: "vikas" };
-  arr.push(obj);
-  if (arr.length == 2) {
-    console.log(arr[0] === arr[1]);
+  let user = useRef({name : "vikas", age : 21})
+
+  const incrementAge = ()=>{ 
+    user.current.age++;
+    console.log(user);
   }
 
   return (
-    <div>
-      <h1>Count : {count}</h1>
+    <div className="App"> 
+    <p>
+      Name: <b>{user.current.name}</b>
+      Age: <b>{user.current.age}</b>
+    </p>
+      <h1>Count : {count}</h1> 
+      <button onClick={ incrementAge()}>Incremrent Age</button>
       <button onClick={() => setCount(count + 1)}>Increment</button>
-
     </div>
   )
 

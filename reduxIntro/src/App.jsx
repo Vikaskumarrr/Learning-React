c3b28e0d9aa7f66455b3987e6dc0037656b9895c
Vikaskumarrr <kashyupvikas2@gmail.com>
2025-01-23 21:09:18 +0530

    deepdiveIntoRedux

---
+++ reduxIntro/src/App.jsx
@@ -0,0 +1,16 @@
+
+import store from "./store.jsx";
+
+const App = ()=>{ 
+
+
+    return (
+        <div>
+            <h1>Redux</h1>
+        </div>
+    )
+
+
+};
+
+export default App ; 
---
+++ reduxIntro/src/store.jsx
@@ -0,0 +1,36 @@
+// create store 
+
+import { legacy_createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+/**
+ * 1. action : its a plan javascript object . {type: "do_something"}
+ * 2. reducer : its a plain javascript function that takes two arguments previosState and action then return the updated state.
+ * 3. subscriber : its a plain javscipt functino that will get executed whenever there a change in the "state".
+ * 
+ */
+
+
+const reducer = function (state = { count: 10 }, action) {
+    if (action.type === "increment") {
+        return { count: state.count + 1 };
+    }
+    if (action.type === "decrement") {
+        return { count: state.count - 1 }
+    }
+    return state;
+}
+
+const store = legacy_createStore(reducer, composeWithDevTools());
+console.log(store);
+
+console.log(store.getState());
+store.dispatch({ type: "increment" });
+console.log(store.getState());
+store.dispatch({ type: "decrement" });
+console.log(store.getState());
+
+
+
+
+export default store;
